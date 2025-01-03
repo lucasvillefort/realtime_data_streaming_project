717620a8ff44eece49b6133a488ccc9857290278
lucasvillefort <lucasvillefort11@gmail.com>
2025-01-03 16:40:24 +0000

    dfd

---
+++ script/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+if [ -e "/opt/airflow/requirements.txt" ]; then
+  $(command python) pip install --upgrade pip
+  $(command -v pip) install --user -r requirements.txt
+fi
+# Initialize the database and create the admin user if it doesn't exist for airflow webserver to run localhost:8080
+if [ ! -f "/opt/airflow/airflow.db" ]; then
+  airflow db init && \
+  airflow users create \
+    --username admin \
+    --firstname admin \
+    --lastname admin \
+    --role Admin \
+    --email admin@example.com \
+    --password admin
+fi
+
+$(command -v airflow) db upgrade
+
+exec airflow webserver
